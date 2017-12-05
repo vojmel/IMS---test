2fd093d48d88e6ca6aa2422947c2a106d2def51a
Unknown <vojmel@gmail.com>
2017-12-05 23:46:29 +0100

    yvdds

--- src/testapp/testFilterSpeed.java
+++ src/testapp/testFilterSpeed.java
@@ -204,7 +204,7 @@ public class testFilterSpeed {
         }
         
         String selectTableSQL = "SELECT "+coloumns+" from "+tableName+ " WHERE "+where;
-        //System.out.println("SQL:"+selectTableSQL);
+        System.out.println("SQL: "+selectTableSQL);
         
         Statement statement = null;
         
